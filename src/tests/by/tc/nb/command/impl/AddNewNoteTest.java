735b7b698865e6361c766036304de575d5e987cd
IvanAksionau <Ivan_Aksionau@epam.com>
2016-10-06 15:10:49 +0300

    create test directory

---
+++ src/tests/by/tc/nb/command/impl/AddNewNoteTest.java
@@ -0,0 +1,24 @@
+package by.tc.nb.command.impl;
+
+import org.junit.Before;
+
+/**
+ * Created by Ivan on 06.10.2016.
+ */
+public class AddNewNoteTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void execute() throws Exception {
+
+    }
+
+}
